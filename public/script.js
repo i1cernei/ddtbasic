99d355a50a64a9242b594c32fc3f969106829dba
Ioan Cernei <i@blackandfield.com>
2022-09-27 19:28:49 +0300

    webcamin

--- public/script.js
+++ public/script.js
@@ -1,55 +1,58 @@
 // Put event listeners into place
 window.addEventListener("DOMContentLoaded", function() {
   // Grab elements, create settings, etc.
-  var canvas = document.getElementById("firstCanvas"),
-      context = canvas.getContext("2d"),
-      // we don't need to append the video to the document
-      video = document.createElement("video"),
-      videoObj =
-      navigator.getUserMedia || navigator.mozGetUserMedia ? // our browser is up to date with specs ?
-      {
-      video: {
-          width: { min: 1280,  max: 1280 },
-          height: { min: 720,  max: 720 },
-          require: ['width', 'height']
-          }
-      }:
-      {
-      video: {
-          mandatory: {
-              minWidth: 1280,
-              minHeight: 720,
-              maxWidth: 1280,
-              maxHeight: 720
-          }
-      }
-  },
-  errBack = function(error) {
-      console.log("Video capture error: ", error.code);
-  };
-  // create a crop object that will be calculated on load of the video
-  var crop;
-  // create a variable that will enable us to stop the loop.
-  var raf;
 
-  navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-  // Put video listeners into place
-  navigator.getUserMedia(videoObj, function (stream) {
-    console.log(videoObj);
-          video.srcObject = stream;
-          video.onplaying = function(){
-              var croppedWidth = ( Math.min(video.videoHeight, canvas.height) / Math.max(video.videoHeight,canvas.height)) * Math.min(video.videoWidth, canvas.width),
-              croppedX = ( video.videoWidth - croppedWidth) / 2;
-              crop = {w:croppedWidth, h:video.videoHeight, x:croppedX, y:0};
-              // call our loop only when the video is playing
-              raf = requestAnimationFrame(loop);
-              };
-          video.onpause = function(){
-              // stop the loop
-              cancelAnimationFrame(raf);
-              }
-          video.play();
-      }, errBack);
+  let crop;
+  // // create a variable that will enable us to stop the loop.
+  let raf;
+  const canvas = document.getElementById("firstCanvas"),
+    context = canvas.getContext("2d"),
+    // we don't need to append the video to the document
+    video = document.createElement("video"),
+
+    videoObj = navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: false,
+      }).then(stream => {
+        video.srcObject = stream;
+        video.onplaying = function(){
+        const croppedWidth = ( Math.min(video.videoHeight, canvas.height) / Math.max(video.videoHeight,canvas.height)) * Math.min(video.videoWidth, canvas.width),
+         croppedX = ( video.videoWidth - croppedWidth) / 2;
+        crop = {w:croppedWidth, h:video.videoHeight, x:croppedX, y:0};
+        // call our loop only when the video is playing
+        raf = requestAnimationFrame(loop);
+            };
+        video.onpause = function(){
+            // stop the loop
+            cancelAnimationFrame(raf);
+            }
+        video.play();
+      }).catch(error => {
+        console.log(error);
+      });
+  document.querySelector('body').appendChild(video);
+
+  // // create a crop object that will be calculated on load of the video
+
+
+  // navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+  // // Put video listeners into place
+  // navigator.getUserMedia(videoObj, function (stream) {
+  //   console.log(videoObj);
+  //         video.srcObject = stream;
+  //         video.onplaying = function(){
+  //             var croppedWidth = ( Math.min(video.videoHeight, canvas.height) / Math.max(video.videoHeight,canvas.height)) * Math.min(video.videoWidth, canvas.width),
+  //             croppedX = ( video.videoWidth - croppedWidth) / 2;
+  //             crop = {w:croppedWidth, h:video.videoHeight, x:croppedX, y:0};
+  //             // call our loop only when the video is playing
+  //             raf = requestAnimationFrame(loop);
+  //             };
+  //         video.onpause = function(){
+  //             // stop the loop
+  //             cancelAnimationFrame(raf);
+  //             }
+  //         video.play();
+  //     }, errBack);
 
   function loop(){
      context.drawImage(video, crop.x, crop.y, crop.w, crop.h, 0, 0, canvas.width, canvas.height);
